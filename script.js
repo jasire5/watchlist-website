0147126ac42311ce6bf8281a2f6e782ed520f033
jasire5 <jasire5@nycstudents.net>
2026-01-14 09:45:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 console.log("js console");
-
-let data;
-let grid = document.querySelector('.grid-container');
-
+var data;
+var grid = document.querySelector('.grid-container');
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        data = JSON.parse(this.responseText);
        console.log(data);  
-    }
-    data.ForEach(function(game) {
-        let container = document.createElement('div');
-        container.classList.add("container");
+    
+    data.forEach(function(film) {
+        let card = document.createElement('div');
+        card.classList.add("card");
 
         let textData = 
-        "div class='game-title'>" + game.title + "</div>" +
+        "<div class='title'>" + film.title + "</div>" +
         "<span>" +
-        "publisher: " + game.publisher + "<br>" +
-        "release date: " + game.releaseDate + "<br>" +
-        "genre: " + game.genre + "<br>" +
-        "imdb rating: " + game.imdbRating + "<br>" +
+        "publisher: " + film.publisher + "<br>" +
+        "year: " + film.year + "<br>" +
+        "genre: " + film.genre + "<br>" +
+        "imdb rating: " + film.imdbRating + "<br>" +
         "</span>";
 
-        container.innerHTML = textData;
+        card.innerHTML = textData;
         
-        if (game.imgSrc) {
-            container.style.backgroundImage = "url('" + game.imgSrc + "')";
+        if (film.Images && film.Images.length > 0) {
+            card.style.backgroundImage = "url('" + film.Images[0] + "')";
         }
-        grid.appendChild(container);
-    });
+        grid.appendChild(card); 
+    }); 
+}
 };
-xhttp.open("GET", "games.json", true);
+xhttp.open("GET", "Film.JSON", true);
 xhttp.send();
